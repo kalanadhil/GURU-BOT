f094a0c0939b59a4948ff96d4554babf680e286d
Kalana dhil <146031452+kalanadhil@users.noreply.github.com>
2023-09-28 23:10:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['0776531133', 'kalana', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
